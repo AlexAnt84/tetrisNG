1dd68a09a4148e220e243abe19b7518152659150
Alexey <79995340383@yandex.ru>
2024-05-15 15:46:11 +0300

    Class Rectangle

--- tetrisNG/graphics.py
+++ tetrisNG/graphics.py
@@ -1,3 +1,5 @@
+from ast import main
+from decimal import Decimal
 from pyclbr import Class
 from main import *
 from settings import *
@@ -10,10 +12,15 @@ BLUE = (0,0,255)
 WHITE = (255, 255, 255)
 SCREEN_CENTER = (WINDOW_HEIGHT/2, WINDOW_WIDTH/2)
 
-RECT_SIZE_HEIGHT = 50
-RECT_SIZE_WIDTH = 50
-#RECT_VALUE = RECT_SIZE_X, RECT_SIZE_Y
-    
+RECT_SIZE_HEIGHT = 10
+RECT_SIZE_WIDTH = 10
+
+x_coords = 0
+y_coords = 0    
+
+
+
+################
 
 ################
 # Objects
@@ -27,14 +34,42 @@ main_surface.fill(WHITE)
 main_surface.set_alpha(200)
 ################
 
+#################
+# Class Rectangle
+class Rectangle:
+    def __init__(self, color, x, y):
+#        self.surface = surface
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = RECT_SIZE_WIDTH
+        self.height = RECT_SIZE_HEIGHT
+       
+        
+#    def __str__(self) -> str:
+#        return f'{self.color} rect appears at {self.x},{self.y}. Width {self.width}, Height {self.height}'
+        
+    def draw(self):
+        print (f'{self.color} rect appears at {self.x},{self.y}. Width {self.width}, Height {self.height}')
+        return pg.draw.rect(main_surface, self.color, (self.x,self.y, self.width,self.height))
+
+
+
+
 ################
 # Rect Init
-## TODO Formula
-main_rect = pg.draw.rect(main_surface,BLUE,(512,512 + RECT_SIZE_WIDTH,562,RECT_SIZE_WIDTH/2))
+#
+blue_block = Rectangle(BLUE, 125, 125)
+blue_block.draw()
+    
+#main_rect = pg.draw.rect(main_surface,BLUE,(x_coords,y_coords,RECT_SIZE_WIDTH, RECT_SIZE_HEIGHT))
 ###############
 
 
-    
+
+
+
+
 
 
 
